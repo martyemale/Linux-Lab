3a966cde6136ee9dd3711bf3b56d32644e049c18
Marty Emale <Me100@rice.edu>
2026-06-09 09:07:46 -0500

    Added network info, top CPU processes, logged in users

--- sysinfo.sh
+++ sysinfo.sh
@@ -7,3 +7,12 @@ echo "Shell: $SHELL"
 echo "Disk Usage:"
 df -h /
 echo "Running Processes: $(ps aux | wc -l)"
+echo "========================="
+echo "Network Info:"
+ifconfig en0 | grep "inet "
+echo "========================="
+echo "Top 5 CPU Processes:"
+ps aux | sort -nrk 3,3 | head -5
+echo "========================="
+echo "Logged In Users:"
+who
